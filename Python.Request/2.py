5ce9a4fba76b72620f7aa5d2f5dc7f16965e8e08
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-06-19 15:47:00 +0300

    add

--- Python.Request/2.py
+++ Python.Request/2.py
@@ -1,9 +1,11 @@
 import requests
 import json
+from bs4 import BeautifulSoup
+
 
 response = requests.get('https://www.kufar.by/l/mebel')
 mebel_data = response.text
 data = mebel_data.split('title')
-print(data)
+print()
 # response.raise_for_status()  # raises exception when not a 2xx response
 # if response.status_code != 204:
