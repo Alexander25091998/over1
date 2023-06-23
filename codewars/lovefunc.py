ab3ed5396bb99236b22d6dff4880e9aa422d9a95
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-06-23 19:21:05 +0300

    new file and add

---
+++ codewars/lovefunc.py
@@ -0,0 +1,18 @@
+# def lovefunc(flower1, flower2):
+#      a = flower1 % 2
+#      b = flower2 % 2
+#      if a == 0 and b == 1:
+#          return True
+#      if a == 1 and b == 0:
+#          return True
+#      if a == 0 and b == 0:
+#          return False
+#      if a == 1 and b == 1:
+#          return False
+def lovefunc(flower1, flower2):
+    return print((flower1 + flower2) % 2)
+
+
+
+
+lovefunc(1, 4)
