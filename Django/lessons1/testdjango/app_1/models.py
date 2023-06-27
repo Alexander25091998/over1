d3e417d9d86a861aba4c3072b07fbecc9491b209
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-06-27 18:33:07 +0300

    new file and add

--- Django/lessons1/testdjango/app_1/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Person(models.Model):
-    name = models.CharField('Название авто', max_length=30),
-
--- Django/lessons1/testdjango/app_1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path('1', views.url1),
-    path('1', views.url2)
-]
