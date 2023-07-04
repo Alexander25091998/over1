f8947d35cd3033fb011c12f72ac9814529cbc44a
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-07-04 10:00:49 +0300

    new file and add

---
+++ на проверку/Virtual/testproject/testapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from . import models
+
+
+admin.site.register(models.Mode1)
+admin.site.register(models.Mode2)
---
+++ на проверку/Virtual/testproject/testapp/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Mode1(models.Model):
+    pole1 = models.IntegerField()
+    pole2 = models.CharField(max_length=45)
+    pole3 = models.TextField()
+
+    class Meta:
+        verbose_name = "Model"
+        verbose_name_plural = "Models"
+
+    def __str__(self):
+        return str(self.pole2) + ' ' + str(self.pole1)
+
+
+class Mode2(models.Model):
+    pole1 = models.IntegerField()
+    pole2 = models.CharField(max_length=45)
+    pole3 = models.ForeignKey(Mode1, models.CASCADE)
+
+    class Meta:
+        verbose_name = "Model"
+        verbose_name_plural = "Models"
+
+    def __str__(self):
+        return str(self.pole2) + ' ' + str(self.pole1)
+
+
+
