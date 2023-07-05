36a36015f280d20b377e4c5d3878a60d6163d62c
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-07-05 14:39:14 +0300

    new file and add

--- на проверку/Virtual/website/main/views.py
+++ на проверку/Virtual/website/main/views.py
@@ -6,7 +6,4 @@ def index(request):
     return HttpResponse('<h4>Hello</h4>')
 
 
-def user(request):
-    return HttpResponse('<a href="https://github.com/Alexander25091998">CCылка на мой GitHub</a>')
-
 
