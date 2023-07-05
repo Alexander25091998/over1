9c8c63857a2ac092e61e2c34512620c1dfd10d37
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-07-05 14:36:57 +0300

    new file and add

--- на проверку/Virtual/website/main/views.py
+++ на проверку/Virtual/website/main/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<h4>Hello</h4>')
 
+
+def user(request):
+    return HttpResponse('<a href="https://github.com/Alexander25091998">CCылка на мой GitHub</a>')
+
+
