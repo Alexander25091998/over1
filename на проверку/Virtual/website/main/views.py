52074bc4217806eace1cab38588ae841cadb01f4
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-07-05 15:35:54 +0300

    new file and add

--- на проверку/Virtual/website/main/views.py
+++ на проверку/Virtual/website/main/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h4>Hello</h4>')
+    return render(request, 'index.html')
 
+
+def about(request):
+    return HttpResponse('<h4>About</h4>')
 
 
