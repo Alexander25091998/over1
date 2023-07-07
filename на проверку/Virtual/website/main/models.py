eab2dc479d9c258b46400de8cb8496afc4913b3f
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-07-07 12:40:08 +0300

    new file and add

--- на проверку/Virtual/website/main/models.py
+++ на проверку/Virtual/website/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    date = models.Model("Дата публикации")
+    tittle = models.CharField("Название", max_length=50)
+    task = models.TextField("Описание")
+    contact = models.TextField("Контакты")
+    def __str__(self):
+        return self.tittle
+
--- на проверку/Virtual/website/main/urls.py
+++ на проверку/Virtual/website/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('user', views.user),
-    path('about-us', views.about)
+    path('', views.index, name='home'),
+    path('user', views.user, name='user'),
+    path('about-us', views.about, name='about')
 ]
