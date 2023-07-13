145592edd22328af70e0f07f4a9c1c08cf0f00da
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-07-13 16:17:20 +0300

    new file and add

--- weather/WeatherApp/weather/views.py
+++ weather/WeatherApp/weather/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    return render(request, 'weather/index.html')
+    apiid = "8dccd4367e7a423e71b0d642cf108959"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + apiid
+    city = 'Brest'
+    res = requests.get(url.format(city))
+    data = res.json()
+    city_info = {
+        "city": city,
+        "temp": data["main"]["temp"],
+        "icon": data["weather"][0]["icon"],
+    }
+    context = {"info": city_info}
+    return render(request, 'weather/index.html', context)
