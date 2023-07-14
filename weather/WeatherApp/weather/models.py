ed522aa8456fdfdf4dd7381abf0e259b19bcb52c
Alexander25091998 <yoursecretadmirer343@gmail.com>
2023-07-14 15:48:39 +0300

    add

--- weather/WeatherApp/weather/models.py
+++ weather/WeatherApp/weather/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    city = models.CharField("Название города", max_length=15)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.city
+
+    class Meta:
+        verbose_name = "Город"
+        verbose_name_plural = "Города"
--- weather/WeatherApp/weather/views.py
+++ weather/WeatherApp/weather/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
+from .models import City
 import requests
 
 
 def index(request):
-    apiid = "8dccd4367e7a423e71b0d642cf108959"
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + apiid
-    city = 'Brest'
-    res = requests.get(url.format(city))
-    data = res.json()
-    city_info = {
-        "city": city,
-        "temp": data["main"]["temp"],
-        "icon": data["weather"][0]["icon"],
-    }
-    context = {"info": city_info}
-    return render(request, 'weather/index.html', context)
+    for e in City.objects.all():
+        cit = City.objects.all()
+        city = e
+        apiid = "8dccd4367e7a423e71b0d642cf108959"
+        url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + apiid
+        res = requests.get(url.format(city))
+        data = res.json()
+        city_info = {
+            "city": city,
+            "temp": data["main"]["temp"],
+            "icon": data["weather"][0]["icon"],
+        }
+        return render(request, 'weather/index.html',  {'city': cit, "city_info": city_info})
